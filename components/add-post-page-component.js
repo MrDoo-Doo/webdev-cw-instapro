3943afeeed188eb0a3aea343f98e743bb55afe57
Алексей <efremovalexei02@gmail.com>
2025-11-01 15:40:01 +0500

    ferstacc

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,49 +1,46 @@
+import { USER_POSTS_PAGE } from "../routes.js";
+import { renderHeaderComponent } from "./header-component.js";
+import { posts, goToPage } from "../index.js";
+import { renderUploadImageComponent } from "./upload-image-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
+  let imageUrl = "";
+
   const render = () => {
     // @TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
       Cтраница добавления поста
-      <div class="upload-image-container">
-      <div class="upload-image">
-            <label class="file-upload-label secondary-button">
-              <input
-                type="file"
-                class="file-upload-input"
-                style="display:none"
-              />
-              Выберите фото
-            </label>
-        }
-      </div>
-      </div>
+      <div class="upload-image-container"></div>
       <input type="text" id="name-input" class="input" placeholder="Описание" />
       <button class="button" id="add-button">Добавить</button>
     </div>
   `;
 
-    // ${
-    //           imageUrl
-    //             ? `
-    //             <div class="file-upload-image-container">
-    //               <img class="file-upload-image" src="${imageUrl}" alt="Загруженное изображение">
-    //               <button class="file-upload-remove-button button">Заменить фото</button>
-    //             </div>
-    //             `
-    //             : `
-    //             <label class="file-upload-label secondary-button">
-    //               <input
-    //                 type="file"
-    //                 class="file-upload-input"
-    //                 style="display:none"
-    //               />
-    //               Выберите фото
-    //             </label>
-    //           `
-    //         }
-
     appEl.innerHTML = appHtml;
+
+    renderHeaderComponent({
+      element: document.querySelector(".header-container"),
+    });
+
+    const uploadImageContainer = appEl.querySelector(".upload-image-container");
+    if (uploadImageContainer) {
+      renderUploadImageComponent({
+        element: uploadImageContainer,
+        onImageUrlChange(newImageUrl) {
+          imageUrl = newImageUrl;
+        },
+      });
+    }
+
+    for (let userEl of document.querySelectorAll(".post-header")) {
+      userEl.addEventListener("click", () => {
+        goToPage(USER_POSTS_PAGE, {
+          userId: userEl.dataset.userId,
+        });
+      });
+    }
 
     document.getElementById("add-button").addEventListener("click", () => {
       onAddPostClick({
